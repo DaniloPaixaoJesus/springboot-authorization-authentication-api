0652c8c30cc5f7ccdce8ec76fe60d134de64c6ea
Danilo <danilo.paixao@gingaone.com>
2019-02-27 18:42:15 -0300

    remove no need files

--- src/main/java/br/com/danilopaixao/ws/detail/DetailEntityRepository.java
+++
@@ -1,6 +0,0 @@
-package br.com.danilopaixao.ws.detail;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface DetailEntityRepository extends JpaRepository<DetailEntity, Long> {
-}
--- src/main/java/br/com/danilopaixao/ws/master/MasterEntityRepository.java
+++
@@ -1,6 +0,0 @@
-package br.com.danilopaixao.ws.master;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface MasterEntityRepository extends JpaRepository<MasterEntity, Long> {
-}
